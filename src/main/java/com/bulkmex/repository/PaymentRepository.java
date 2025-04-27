6f88d37ac919b489d3334fb00be41cc9bd4fa6c5
AnuarOrozco <anuarorozzco@gmail.com>
2025-04-27 17:11:53 -0600

    refactor(payment): Change findByOrderId method to use Long instead of Order

--- src/main/java/com/bulkmex/repository/PaymentRepository.java
+++ src/main/java/com/bulkmex/repository/PaymentRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByOrder(Order order);
+
+    Optional<Payment> findByOrderId(Long orderId);
 }
