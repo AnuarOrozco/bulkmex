9f386227402700a79ed81daa90535e4df16de3ae
AnuarOrozco <anuarorozzco@gmail.com>
2025-04-26 22:43:51 -0600

    Base project for BULKMEX

---
+++ src/main/java/com/bulkmex/BulkmexApplication.java
@@ -0,0 +1,13 @@
+package com.bulkmex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkmexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulkmexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulkmex/BulkmexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulkmex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkmexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
