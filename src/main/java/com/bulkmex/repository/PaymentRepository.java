1eb9d6fe260e38455ac58f912cd364d681a3d69e
AnuarOrozco <anuarorozzco@gmail.com>
2025-04-27 13:14:19 -0600

    feat(payment): add findByOrder method to PaymentRepository

--- src/main/java/com/bulkmex/repository/PaymentRepository.java
+++ src/main/java/com/bulkmex/repository/PaymentRepository.java
@@ -1,7 +1,11 @@
 package com.bulkmex.repository;
 
+import com.bulkmex.model.Order;
 import com.bulkmex.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrder(Order order);
 }
